ed3b29af046e6d9a42ec60e34310e3b80b686799
antoniab79 <antonia@webleviathan.com>
2024-11-12 14:53:15 +0000

    added jS file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello world");
+function generateQoute(event) {
+    event.preventDefault();
+
+    new Typewriter("#qoute", {
+        strings: response.data.answer,
+        autoStart: true,
+        cursor: null,
+        delay: 30,
+    });
+}
+
+let qouteFormElement = document.querySelector("#qoute-generator-form");
+qouteFormElement.addEventListener("submit", generateQoute);
